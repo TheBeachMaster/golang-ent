0e579194804f8e6be5f966f141d3df119056a6a4
TheBeachmaster <otienoken@outlook.com>
2022-01-12 11:12:19 +0300

    Add Song HTTP Handlers Add Song UseCase

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"log"
 
 	"com.thebeachmaster/entexample/config"
+	"com.thebeachmaster/entexample/ent"
 	"com.thebeachmaster/entexample/server"
 )
 
@@ -22,7 +24,22 @@ func main() {
 		log.Fatalf("ParseConfig Error: %v", err)
 	}
 
-	server := server.NewServer(cfg)
+	// postgresql://user:password@host/database
+	// TODO: Handle Heroku DB Conn String
+	dbConnString := "postgresql://" + cfg.DB.DBUser + ":" + cfg.DB.DBPass + "@" + cfg.DB.DBHost + "/" + cfg.DB.DBName
+
+	client, err := ent.Open("postgres", dbConnString)
+	if err != nil {
+		log.Fatalf("failed opening connection to sqlite: %v", err)
+	}
+	defer client.Close()
+	// Run the auto migration tool.
+	if err := client.Schema.Create(context.Background()); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+
+	// Add ent Client
+	server := server.NewServer(cfg, client)
 	if err = server.Run(); err != nil {
 		log.Fatal(err)
 	}
